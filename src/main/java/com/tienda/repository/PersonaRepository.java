6d5b61c3462454ea3a5fc6b2af9d140322e846d4
Kreyes450 <reyesk450@gmail.com>
2023-02-23 15:39:04 -0600

    Parte de Semana 6

--- src/main/java/com/tienda/repository/PersonaRepository.java
+++ src/main/java/com/tienda/repository/PersonaRepository.java
@@ -1,19 +1,15 @@
 
 package com.tienda.repository;
 
-import com.tienda.entity.Pais;
 import com.tienda.entity.Persona;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 
-public interface PersonaRepository extends CrudRepository<Pais, Long> {
+public interface PersonaRepository extends CrudRepository<Persona, Long> {
 
     public Object findAllById(long id);
 
-    public void save(Persona persona);
-
     public void deleteAllById(long id);
- 
 }
